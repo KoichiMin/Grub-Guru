9fe98a0c3adc8990974775a254864e36f5ec1a76
“Koichi <koichidminami@gmail.com>
2023-04-08 14:54:24 -0400

    added files to begin boxes in homepage

---
+++ client/src/Components/CalorieBox.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+
+const CalorieBox = () =>{
+    return(
+        <Wrapper> CalorieBox</Wrapper> 
+    )
+}
+
+const Wrapper = styled.div`
+`
+
+export default CalorieBox
---
+++ client/src/Components/FavouriteFoods.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+
+const FavouriteFoods = () =>{
+    return(
+        <Wrapper> FavouriteFoods</Wrapper> 
+    )
+}
+
+const Wrapper = styled.div`
+`
+
+export default FavouriteFoods
